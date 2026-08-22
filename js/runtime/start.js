ca5299e75c387e2587a3a9e32c3e634537ca4e0f
Dmitrii Rogoznyi <k911mipt@gmail.com>
2026-08-22 16:52:59 +0300

    feat: add Terraria-style browser favicon
    Add a local pixel-art tree-on-grass favicon and connect it to the planner without external image dependencies.

--- js/runtime/start.js
+++ js/runtime/start.js
@@ -1,3 +1,11 @@
+// Browser-tab icon. Kept local so the page does not depend on wiki.gg assets.
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/svg+xml";
+favicon.href = "./favicon.svg";
+favicon.sizes = "any";
+document.head.append(favicon);
+
 // Cache construction, table population and initial arena focus.
 buildBaseCaches();
 
