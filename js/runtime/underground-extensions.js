751b9b13b540be87845c7936674633addeb3ec1e
Dmitrii Rogoznyi <k911mipt@gmail.com>
2026-08-24 19:26:17 +0300

    fix: render Goblin workshop decor accurately

--- js/runtime/underground-extensions.js
+++ js/runtime/underground-extensions.js
@@ -94,6 +94,96 @@ function drawUndergroundWater(ctx, o) {
   ctx.restore();
 }
 
+function drawGoblinGreenTorch(ctx, o) {
+  const b = objectBox(o),
+    cx = b.x + b.w / 2;
+  ctx.save();
+  ctx.globalAlpha = 0.32;
+  ctx.fillStyle = "#78e86f";
+  ctx.beginPath();
+  ctx.arc(cx, b.y + b.h / 2, 11, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.restore();
+  pxRect(ctx, cx - 1, b.y + 6, 2, 8, "#68482f");
+  ctx.fillStyle = "#d8ffc4";
+  ctx.beginPath();
+  ctx.moveTo(cx, b.y + 1);
+  ctx.lineTo(cx - 4, b.y + 8);
+  ctx.lineTo(cx, b.y + 11);
+  ctx.lineTo(cx + 4, b.y + 8);
+  ctx.closePath();
+  ctx.fill();
+  ctx.fillStyle = "#62cf58";
+  ctx.beginPath();
+  ctx.moveTo(cx, b.y + 3);
+  ctx.lineTo(cx - 2, b.y + 8);
+  ctx.lineTo(cx, b.y + 9);
+  ctx.lineTo(cx + 2, b.y + 8);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function drawGoblinCopperChandelier(ctx, o) {
+  const b = objectBox(o),
+    cx = b.x + b.w / 2,
+    top = b.y + 1,
+    barY = b.y + 13;
+  ctx.save();
+  ctx.globalAlpha = 0.2;
+  ctx.fillStyle = "#ffc273";
+  ctx.beginPath();
+  ctx.arc(cx, b.y + b.h * 0.58, 23, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.restore();
+
+  pxRect(ctx, cx - 1, top, 2, 9, "#69412f");
+  pxRect(ctx, b.x + 7, barY, b.w - 14, 3, "#a9633d");
+  pxRect(ctx, b.x + 9, barY + 1, b.w - 18, 1, "#dc9862");
+
+  for (const offset of [-13, 0, 13]) {
+    const lampX = cx + offset;
+    pxRect(ctx, lampX - 1, barY + 3, 2, 5, "#69412f");
+    pxRect(ctx, lampX - 4, barY + 8, 8, 6, "#a9633d");
+    pxRect(ctx, lampX - 3, barY + 9, 6, 4, "#ffd078");
+    pxRect(ctx, lampX - 2, barY + 10, 4, 3, "#fff0b5");
+  }
+}
+
+function drawGoblinToolFrame(ctx, o) {
+  const b = objectBox(o),
+    cx = b.x + b.w / 2,
+    cy = b.y + b.h / 2;
+  pxRect(ctx, b.x + 2, b.y + 2, b.w - 4, b.h - 4, "#5b3a26");
+  pxRect(ctx, b.x + 4, b.y + 4, b.w - 8, b.h - 8, "#bc7a4e");
+  pxRect(ctx, b.x + 6, b.y + 6, b.w - 12, b.h - 12, "#26322b");
+
+  ctx.save();
+  ctx.strokeStyle = "#d9e1cf";
+  ctx.lineWidth = 3;
+  ctx.lineCap = "round";
+  if (o.id === "UG_TOOL_FRAME_WRENCH") {
+    ctx.beginPath();
+    ctx.moveTo(cx - 6, cy + 6);
+    ctx.lineTo(cx + 4, cy - 4);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(cx + 6, cy - 6, 4, 0.3, Math.PI * 1.7);
+    ctx.stroke();
+  } else {
+    ctx.beginPath();
+    ctx.moveTo(cx - 7, cy - 7);
+    ctx.lineTo(cx + 6, cy + 6);
+    ctx.moveTo(cx + 7, cy - 7);
+    ctx.lineTo(cx - 6, cy + 6);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(cx - 6, cy + 7, 3, 0, Math.PI * 2);
+    ctx.arc(cx + 6, cy + 7, 3, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+  ctx.restore();
+}
+
 const sharedUndergroundDrawPylon = drawPylon;
 drawPylon = function drawCavernPylon(ctx, o) {
   if (o.style !== "cavern_pylon") {
@@ -135,8 +225,18 @@ drawPylon = function drawCavernPylon(ctx, o) {
   pxRect(ctx, b.x + 3, b.y + b.h - 5, b.w - 6, 4, "#4b5055");
 };
 
+const sharedUndergroundDrawLight = drawLight;
+drawLight = function drawUndergroundLight(ctx, o) {
+  if (o.id === "UG_GOBLIN_GREEN_TORCH") return drawGoblinGreenTorch(ctx, o);
+  if (o.id === "UG_GOBLIN_CHANDELIER") return drawGoblinCopperChandelier(ctx, o);
+  return sharedUndergroundDrawLight(ctx, o);
+};
+
 const sharedUndergroundDrawObjectSprite = drawObjectSprite;
 drawObjectSprite = function drawUndergroundObjectSprite(ctx, o) {
   if (o.kind === "water") return drawUndergroundWater(ctx, o);
+  if (o.id === "UG_TOOL_FRAME_WRENCH" || o.id === "UG_TOOL_FRAME_CUTTER") {
+    return drawGoblinToolFrame(ctx, o);
+  }
   return sharedUndergroundDrawObjectSprite(ctx, o);
 };
